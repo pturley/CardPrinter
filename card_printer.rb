0eaa16c5ce58e1b1b53c8935afa0b347b33aa809
Craig Wattrus <cwattrus@thoughtworks.com>
2012-08-23 14:28:18 +0200

    Craig: Fixed access to the MingleHosting instance, added xml parsing and templating

--- card_printer.rb
+++ card_printer.rb
@@ -1,9 +1,30 @@
 require 'rubygems'
 require 'sinatra'
 require 'mingle4r'
+require 'nokogiri'
+require 'open-uri'
 
-get '/:username/:password' do
-  mingle = MingleClient.new('http://minglehosting.thoughtworks.com/mpedigree_console/', params[:username], params[:password])
-  mingle.proj_id = 'mpedigree_console'
-  mingle.cards.inspect
+get '/:username/:password/:story' do
+  @doc = Nokogiri::XML(open("https://minglehosting.thoughtworks.com/mpedigree_console/api/v2/projects/mpedigree_console/cards/" + params[:story] + ".xml", :http_basic_authentication=>[params[:username], params[:password]]))
+  
+
+  "<html>
+  <head>
+    <title>Story Wall | Card Printer</title>
+    <link rel='stylesheet' href='http://www.wattrus.co.za/MinglePrinter/css/StoryWall.css' />
+  </head>
+  <body>
+  <div class='story-wall'>
+    <span class='title'>Story Wall</span>
+    <hr />
+    <span class='links'>Card Printer</span>
+  </div>
+  <div class='Card' id='printable'>
+    <textarea class='title' id='card_title'>" + @doc.xpath("//name")[0] + "</textarea>
+    <input type='text' class='number' name=' value='#" + @doc.xpath("//number")[0] + "' />
+    <input type='text' class='size' name=' value='M' />
+    <input type='text' class='status' name=' value='M' />
+  </div>
+  </body>
+  </html>"
 end
