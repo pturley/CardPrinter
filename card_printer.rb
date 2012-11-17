7982a61afe13b0fef99b871580c9de4a49313484
Craig Wattrus <cwattrus@thoughtworks.com>
2012-11-17 17:10:27 +0200

    Craig: Now using Noko Giri and added layout and style

--- card_printer.rb
+++ card_printer.rb
@@ -6,6 +6,13 @@ require 'open-uri'
 
 get '/:username/:password/:story' do
   @doc = Nokogiri::XML(open("https://minglehosting.thoughtworks.com/mpedigree_console/api/v2/projects/mpedigree_console/cards/" + params[:story] + ".xml", :http_basic_authentication=>[params[:username], params[:password]]))
+  @doc.xpath("//name")[0]
+end
+
+post '/' do
+  puts [params[:username], params[:password]]
+  @doc = Nokogiri::XML(open("https://minglehosting.thoughtworks.com/mpedigree_console/api/v2/projects/mpedigree_console/cards/" + params[:story] + ".xml", :http_basic_authentication=>[params[:username], params[:password]]))
+  @doc.xpath("//name")[0]
   "<html>
   <head>
     <title>Story Wall | Card Printer</title>
@@ -20,9 +27,23 @@ get '/:username/:password/:story' do
   <div class='Card' id='printable'>
     <textarea class='title' id='card_title'>" + @doc.xpath("//name")[0] + "</textarea>
     <input type='text' class='number' value='#" + @doc.xpath("//number")[0] + "' />
-    <input type='text' class='size' value='M' />
-    <input type='text' class='status' value='M' />
+    <input type='text' class='size' value='N/A' />
   </div>
   </body>
-  </html>"
+</html>"
 end
+
+get '/CardPrinter' do
+  "<html>
+    <body>
+    <form name='input' action='http://127.0.0.1:4567/' method='post'>
+      <input name='username' type='text'>
+      <input name='password' type='password'>
+      <input name='story' type='text'>
+      <input type='submit' text='Go'> 
+    </form>
+    </body>
+  </html>
+  "
+end
+
