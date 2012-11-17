c7f4f50029bc8a106dd71197c3b57fa0176aa4c0
Patrick Turley [pturley] <patrick.turley@gmail.com>
2012-11-17 17:49:18 +0200

    Turley: changing to haml

--- card_printer.rb
+++ card_printer.rb
@@ -4,46 +4,20 @@ require 'mingle4r'
 require 'nokogiri'
 require 'open-uri'
 
-get '/:username/:password/:story' do
-  @doc = Nokogiri::XML(open("https://minglehosting.thoughtworks.com/mpedigree_console/api/v2/projects/mpedigree_console/cards/" + params[:story] + ".xml", :http_basic_authentication=>[params[:username], params[:password]]))
-  @doc.xpath("//name")[0]
-end
-
 post '/' do
-  puts [params[:username], params[:password]]
-  @doc = Nokogiri::XML(open("https://minglehosting.thoughtworks.com/mpedigree_console/api/v2/projects/mpedigree_console/cards/" + params[:story] + ".xml", :http_basic_authentication=>[params[:username], params[:password]]))
-  @doc.xpath("//name")[0]
-  "<html>
-  <head>
-    <title>Story Wall | Card Printer</title>
-    <link rel='stylesheet' href='http://www.wattrus.co.za/MinglePrinter/css/StoryWall.css' />
-  </head>
-  <body>
-  <div class='story-wall'>
-    <span class='title'>Story Wall</span>
-    <hr />
-    <span class='links'>Card Printer</span>
-  </div>
-  <div class='Card' id='printable'>
-    <textarea class='title' id='card_title'>" + @doc.xpath("//name")[0] + "</textarea>
-    <input type='text' class='number' value='#" + @doc.xpath("//number")[0] + "' />
-    <input type='text' class='size' value='N/A' />
-  </div>
-  </body>
-</html>"
+  puts params
+  username = params[:username]
+  password = params[:password]
+  number = params[:card_number]
+  doc = Nokogiri::XML(
+    open("https://minglehosting.thoughtworks.com/mpedigree_console/api/v2/projects/mpedigree_console/cards/#{number}.xml", 
+      :http_basic_authentication=>[username, password]))
+  @name = doc.xpath("//name")[0].text
+  @number = doc.xpath("//number")[0].text
+  haml :home
 end
 
-get '/CardPrinter' do
-  "<html>
-    <body>
-    <form name='input' action='http://127.0.0.1:4567/' method='post'>
-      <input name='username' type='text'>
-      <input name='password' type='password'>
-      <input name='story' type='text'>
-      <input type='submit' text='Go'> 
-    </form>
-    </body>
-  </html>
-  "
+get '/card_printer' do
+  haml :card_printer
 end
 
